92e0e82ee484c043cf6c518f007836b79e967fb6
nehang28 <nehangpatel0028@gmail.com>
2024-10-19 23:01:43 +0530

    Adding splash screen

--- app/src/main/java/com/example/eventapplication/MainActivity.java
+++ app/src/main/java/com/example/eventapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.eventapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,10 +23,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.pop_up);
+        logoImageView.startAnimation(fadeInAnimation);
         auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
 
-        if (currentUser != null) {
+        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                navigateToNextActivity(currentUser);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        /*if (currentUser != null) {
             // User is signed in, check their role
             databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(currentUser.getUid());
             databaseReference.child("role").get().addOnCompleteListener(task -> {
@@ -41,6 +59,32 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                     startActivity(intent);
                 }
+                finish();
+            });
+        } else {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+            finish();
+        }*/
+    }
+    private void navigateToNextActivity(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            // User is signed in, check their role
+            databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(currentUser.getUid());
+            databaseReference.child("role").get().addOnCompleteListener(task -> {
+                Intent intent;
+                if (task.isSuccessful() && task.getResult() != null) {
+                    String role = task.getResult().getValue(String.class);
+                    if ("Organizer".equals(role)) {
+                        intent = new Intent(MainActivity.this, Organizer.class);
+                    } else {
+                        intent = new Intent(MainActivity.this, DashboardActivity.class);
+                    }
+                } else {
+                    // Handle case where fetching the role fails
+                    intent = new Intent(MainActivity.this, SignupActivity.class);
+                }
+                startActivity(intent);
                 finish();
             });
         } else {
